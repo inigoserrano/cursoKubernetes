ef39aab549497454f0fd1fbe8f82927eda4c8dea
Iñigo Serrano <contacto@inigoserrano.com>
2019-05-25 13:38:04 +0200

    Bloque 12

--- Bloque11Aplicacion/aplicacion/src/main/java/com/inigoserrano/docker/aplicacion/LivenessProbe.java
+++ Bloque11Aplicacion/aplicacion/src/main/java/com/inigoserrano/docker/aplicacion/LivenessProbe.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
 
 @WebServlet({ "/livenessProbe" })
 public class LivenessProbe extends HttpServlet {
@@ -17,10 +19,21 @@ public class LivenessProbe extends HttpServlet {
      */
     private static final long serialVersionUID = 8532332299687592651L;
 
+    private static final Date inicio = new Date();
+
+    private static final long espera = 5 * 60 * 1000;
+
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
         final PrintWriter writer = response.getWriter();
+
+        Date ahora = new Date();
+        if (inicio.getTime() + espera > ahora.getTime()){
+            writer.println("OK");
+        }else{
+            response.sendError(500);
+        }
         writer.println("OK");
         writer.flush();
     }
--- Bloque11Aplicacion/aplicacion/src/main/java/com/inigoserrano/docker/aplicacion/ReadynessProbe.java
+++ Bloque11Aplicacion/aplicacion/src/main/java/com/inigoserrano/docker/aplicacion/ReadynessProbe.java
@@ -9,6 +9,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet({ "/readynessProbe" })
 public class ReadynessProbe extends HttpServlet {
@@ -18,21 +31,38 @@ public class ReadynessProbe extends HttpServlet {
      */
     private static final long serialVersionUID = 8532332299687592651L;
 
-    private static final Date inicio = new Date();
 
-    private static final long espera = 1 * 60 * 1000;
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
+
         final PrintWriter writer = response.getWriter();
-        Date ahora = new Date();
-        if (inicio.getTime() + espera > ahora.getTime()){
-            writer.println("OK");
-        }else{
+        try {
+            final Connection connection = this.connectDatabase();
+            final Statement stmt = connection.createStatement();
+            final ResultSet resultSet = stmt.executeQuery("SELECT * FROM Personas");
+            if (resultSet.next()) {
+                System.out.println("OK");
+                writer.println("OK");
+            }
+            stmt.close();
+            connection.close();
+
+        } catch (final ClassNotFoundException e) {
             response.sendError(500);
-        } 
+            System.out.println("No OK");
+        } catch (final SQLException e) {
+            response.sendError(500);
+            System.out.println("No OK");
+        }
+
         writer.flush();
+    }
+
+    private Connection connectDatabase() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://mariadb-service:3306/curso", "root", "admin123");
     }
 
 }
