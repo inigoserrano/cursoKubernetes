8f420d2129f56ec77ce1526082796bb62982de84
Iñigo Serrano <contacto@inigoserrano.com>
2019-06-08 09:55:45 +0200

    Limpieza

--- Bloque11Aplicacion/aplicacion/src/main/java/com/inigoserrano/docker/aplicacion/ReadynessProbe.java
+++ Bloque11Aplicacion/aplicacion/src/main/java/com/inigoserrano/docker/aplicacion/ReadynessProbe.java
@@ -2,15 +2,6 @@ package com.inigoserrano.docker.aplicacion;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
